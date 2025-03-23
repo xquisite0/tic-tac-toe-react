02960ac9653868d55f8b2027fbef8e076cca868b
Zhou Yuhang <zyuhang255@gmail.com>
2025-03-23 10:48:10 +0800

    Fix syntax

--- src/App.js
+++ src/App.js
@@ -54,8 +54,8 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const { xIsNext, setXIsNext } = useState(true);
-  const { history, setHistory } = useState([Array[9].fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
